c1d5854e0d382f5e7c1de99d689c0c93b2f0bc03
joshchiou <joshchiou@ucsd.edu>
2017-07-17 23:08:26 -0700

    update bulk atac-seq

--- bulk_ATAC-seq/bulk_ATAC-seq_pipeline.py
+++ bulk_ATAC-seq/bulk_ATAC-seq_pipeline.py
@@ -8,7 +8,7 @@ import reprlib
 import logging
 
 #=======================================================#
-# trim raw reads with trim_galore!
+
 def trim_galore(args):
 	trim_galore_cmd = ['trim_galore', '--fastqc', '-q', '10', '-o', args.output, '--paired', args.paired1, args.paired2]
 	with open(os.devnull, 'w') as f:
@@ -18,19 +18,17 @@ def trim_galore(args):
 	return trim_output_1, trim_output_2
 
 #=======================================================#
-# align reads to the reference genome after throwing 
-# out bad quality reads using samtools view.
-# remove duplicate reads using samtools rmdup.
+
 def process_reads(args):
 	output_prefix = os.path.join(args.output, args.name)
 	align_log = output_prefix + '.align.log'
 	aligned_bam = output_prefix + '.sort.filt.bam'
 	rmdup_bam = output_prefix + '.sort.filt.rmdup.bam'
 
-	bwa_mem_cmd = ['bwa', 'mem', '-M', '-t', str(args.processes), args.reference, args.paired1, args.paired2]
-	quality_filt_cmd = ['samtools', 'view', '-h', '-f', '0x2', '-F', '0x100', '-q', str(args.quality), '-@', str(args.processes), '-']
+	bwa_mem_cmd = ['bwa', 'mem', '-M', '-t', str(args.threads), args.reference, args.paired1, args.paired2]
+	quality_filt_cmd = ['samtools', 'view', '-h', '-f', '0x2', '-F', '0x100', '-q', str(args.quality), '-@', str(args.threads), '-']
 	mito_filt_cmd = ['grep', '-v', 'chrM']
-	samtools_sort_cmd = ['samtools', 'sort', '-m', '{}G'.format(args.memory), '-@', str(args.processes), '-']
+	samtools_sort_cmd = ['samtools', 'sort', '-m', '{}G'.format(args.memory), '-@', str(args.threads), '-']
 	
 	with open(align_log, 'w') as log, open(aligned_bam, 'w') as bam_out:
 		bwa_mem = subprocess.Popen(bwa_mem_cmd, stdout=subprocess.PIPE, stderr=log)
@@ -59,7 +57,7 @@ def process_reads(args):
 	return rmdup_bam
 
 #=======================================================#
-# call peaks using macs2
+
 def call_peaks(args, input_bam):
 	macs2_log = os.path.join(args.output, '.'.join([args.name, 'macs2_callpeaks.log']))
 	macs2_cmd = ['macs2', 'callpeak', 
@@ -76,7 +74,7 @@ def call_peaks(args, input_bam):
 	return
 
 #=======================================================#
-# cleanup operations
+
 def cleanup(args):
 	read_1_name = os.path.basename(args.paired1).split('.fastq.gz')[0]
 	read_2_name = os.path.basename(args.paired2).split('.fastq.gz')[0]
@@ -93,6 +91,35 @@ def cleanup(args):
 	return
 
 #=======================================================#
+
+def make_bedgraph(args):
+	bdgcmp_log = os.path.join(args.output, '.'.join([args.name, 'bdgcmp.log']))
+	bdgcmp_cmd = [
+			'macs2', 'bdgcmp',
+			'-t', os.path.join(args.output, args.name + '_treat_pileup.bdg'),
+			'-c', os.path.join(args.output, args.name + '_control_lambda.bdg'),
+			'-m', 'ppois',
+			'--outdir', args.output,
+			'--o-prefix', args.name,
+			'-p', '0.00001']
+	with open(bdgcmp_log, 'w') as f:
+		subprocess.call(bdgcmp_cmd, stderr=f)
+	
+	bdgcmp_out = os.path.join(args.output, args.name + '_ppois.bdg')
+	
+	label = 'track type=bedGraph name=\"{0}\" description=\"{0}\" visibility=2 color=0,0,0 altColor=0,0,0 autoScale=off maxHeightPixels=64:64:32'.format(args.name)
+	sorted_bdg = os.path.join(args.output, args.name  + '_ppois.sorted.bdg')
+	sort_cmd = ['sort', '-k', '1,1', '-k', '2,2n', bdg]
+	with open(sorted_bdg, 'w') as f:
+		print(label, file=f)
+	with open(sorted_bdg, 'a') as f:
+		subprocess.call(sort_cmd, stdout=f)
+	subprocess.call(['gzip', sorted_bdg])
+	os.remove(bdgcmp_out)
+	return
+
+#=======================================================#
+
 def main(args):
 	logging.info('Starting up.')
 
@@ -114,34 +141,47 @@ def main(args):
 			call_peaks(args, rmdup_bam)
 		except Exception as e:
 			print(repr(e), file=sys.stderr)
+	if not args.skip_bdg:
+		try:
+			logging.info('Generating signal track with MACS2')
+			make_bedgraph(args)
+		except:
+			print(repr(e), file=sys.stderr)
 	logging.info('Cleaning up temporary files.')
-	cleanup(args)
+	if not args.skip_cleanup:
+		cleanup(args)
 	logging.info('Finishing up.')
 	return
 
 #=======================================================#
-# process arguments
+
 def process_args():
-	default_ref_genome='/home/joshchiou/references/ucsc.hg19.fasta'
+	parser = argparse.ArgumentParser(description='Pipeline for ATAC-seq to trim reads, align them to a reference genome, call peaks, and generate a genome browser signal track.')
+	
+	io_group = parser.add_argument_group('I/O arguments')	
+	io_group.add_argument('-p1', '--paired1', required=False, type=str, help='Path to paired reads (1)')
+	io_group.add_argument('-p2', '--paired2', required=False, type=str, help='Path to paired reads (2)')
+	io_group.add_argument('-o', '--output', required=True, type=str, help='Output directory for processed files')
+	io_group.add_argument('-n', '--name', required=False, type=str, default='sample', help='Output sample name to prepend')
+	
+	align_group = parser.add_argument_group('Alignment arguments')
+	align_group.add_argument('-t', '--threads', required=False, type=int, default=4, help='Number of threads to use')
+	align_group.add_argument('-m', '--memory', required=False, type=int, default=8, help='Maximum memory per thread for samtools sort')
+	align_group.add_argument('-q', '--quality', required=False, type=int, default=30, help='Mapping quality cutoff for samtools')
+	align_group.add_argument('-ref', '--reference', required=False, type=str, default='/home/joshchiou/references/ucsc.hg19.fasta', help='Path to reference genome')
+	align_group.add_argument('--picard_mark_dup', required=False, type=str, default='/home/joshchiou/bin/MarkDuplicates.jar', help='Path to picard MarkDuplicates.jar')
 
-	parser = argparse.ArgumentParser(description='Pipeline for ATAC-seq to trim reads, align them to a reference genome, and then call peaks.')
-	parser.add_argument('-p1', '--paired1', required=False, type=str, help='Path to paired reads (1)')
-	parser.add_argument('-p2', '--paired2', required=False, type=str, help='Path to paired reads (2)')
-	parser.add_argument('-o', '--output', required=True, type=str, help='Output directory for processed files')
-	parser.add_argument('-n', '--name', required=False, type=str, default='sample', help='Output sample name to prepend')
-	parser.add_argument('-ref', '--reference', required=False, type=str, default=default_ref_genome, help='Path to reference genome')
-	parser.add_argument('-p', '--processes', required=False, type=int, default=4, help='Number of processes to use')
-	parser.add_argument('-m', '--memory', required=False, type=int, default=8, help='Maximum memory per thread for samtools sort')
-	parser.add_argument('-q', '--quality', required=False, type=int, default=30, help='Mapping quality cutoff for samtools')
-	parser.add_argument('--picard_mark_dup', required=False, type=str, default='/home/joshchiou/bin/MarkDuplicates.jar', help='Path to picard MarkDuplicates.jar')
-	parser.add_argument('--skip_trim', required=False, action='store_true', default=False, help='Skip adapter trimming step')
-	parser.add_argument('--skip_align', required=False, action='store_true', default=False, help='Skip read alignment step')
-	parser.add_argument('--skip_peaks', required=False, action='store_true', default=False, help='Skip calling peaks step')
-
+	skip_group = parser.add_argument_group('Skip processing steps')
+	skip_group.add_argument('--skip_trim', required=False, action='store_true', default=False, help='Skip adapter trimming step')
+	skip_group.add_argument('--skip_align', required=False, action='store_true', default=False, help='Skip read alignment step')
+	skip_group.add_argument('--skip_peaks', required=False, action='store_true', default=False, help='Skip calling peaks step')
+	skip_group.add_argument('--skip_bdg', required=False, action='store_true', default=False, help='Skip making genome browser track')
+	skip_group.add_argument('--skip_cleanup', required=False, action='store_true', default=False, help='Skip cleanup operations')
+	
 	return parser.parse_args()
 
 #=======================================================#
-
-args = process_args()
-logging.basicConfig(format='[%(filename)s] %(asctime)s %(levelname)s: %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
-main(args)
+if __name__ == '__main__':
+	logging.basicConfig(format='[%(filename)s] %(asctime)s %(levelname)s: %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
+	args = process_args()
+	main(args)
